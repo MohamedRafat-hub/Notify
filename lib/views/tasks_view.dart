542bbc1347a02b620839d9b6e133469d3b994807
mohamedrafat <mtag66870@gmail.com>
2025-10-19 17:59:40 +0300

    create floating action button

--- lib/views/tasks_view.dart
+++ lib/views/tasks_view.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:tasky_app/widgets/tasks_view_body.dart';
 
-
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(100),
+        ),
+      ),
       body: TasksViewBody(),
     );
   }
 }
-
-
-
