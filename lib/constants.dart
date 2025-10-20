757393b7cfdf5f8b3d7a3f26730d6a21c05c3631
mohamedrafat <mtag66870@gmail.com>
2025-10-20 10:03:09 +0300

    init hive

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,4 @@
 import 'dart:ui';
 
 const KPrimaryColor = Color(0xFF84d9d4);
+const KNotesBox = 'notes_box';
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:tasky_app/constants.dart';
+import 'package:tasky_app/models/note_model.dart';
 import 'package:tasky_app/views/notes_view.dart';
 
-void main()
+void main() async
 {
+  await Hive.initFlutter();
+  await Hive.openBox<NoteModel>(KNotesBox);
   runApp(TaskyApp());
 }
 
